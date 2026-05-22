9e90d182375c1e63971fc47c366f551c91d469c4
Ogun Gundogdu <oguneg@gmail.com>
2026-05-22 15:52:32 +0200

    TMP import and dialogue system

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class DialogueManager : MonoSingleton<DialogueManager>
+{
+    [SerializeField] private DialogueBatch[] dialogueBatches;
+    [SerializeField] private DialoguePanel dialoguePanel;
+    private bool _isWaitingForInput = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            _isWaitingForInput = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(DialogueRoutine(dialogueBatches[0]));
+        }
+    }
+    
+    private IEnumerator DialogueRoutine(DialogueBatch batch)
+    {
+        dialoguePanel.ShowPanel();
+        yield return new WaitForSeconds(0.2f);
+        var dialogue = batch;
+        WaitWhile waitForInput = new WaitWhile(() => _isWaitingForInput);
+        int i = 0;
+        while (true)
+        {
+            if (dialoguePanel.IsAnimatingText)
+            {
+                dialoguePanel.RushMessage(batch.messages[i-1]);
+            }
+            else
+            {
+                if (i >= batch.messages.Length) break;
+                dialoguePanel.ShowMessage(batch.messages[i]);
+                i++;
+            }
+            _isWaitingForInput = true;
+            yield return waitForInput;
+        }
+        dialoguePanel.HidePanel();
+    }
+}
+
+[Serializable]
+public struct DialogueBatch
+{
+    public string[] messages;
+}
---
+++ Assets/Scripts/DialoguePanel.cs
@@ -0,0 +1,56 @@
+using System;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialoguePanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textField;
+    [SerializeField] private Image panelBG;
+    public bool IsAnimatingText { get; private set; } = false;
+
+    public void ShowPanel()
+    {
+        panelBG.transform.localScale = Vector3.zero;
+        textField.text = String.Empty;
+        panelBG.gameObject.SetActive(true);
+        panelBG.transform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
+    }
+
+    public void HidePanel()
+    {
+        panelBG.transform.DOScale(0, 0.3f).SetEase(Ease.InBack)
+            .OnComplete(() => panelBG.gameObject.SetActive(false));
+    }
+
+    public void ShowMessage(string message)
+    {
+        string animatedStr=String.Empty;
+        int animatedStrLength = 0;
+        IsAnimatingText = true;
+        var typewriterTween = DOTween.To(() => animatedStr, x => animatedStr = x, message, 18).SetSpeedBased().SetEase(Ease.Linear).SetTarget(textField)
+            .OnUpdate(() =>
+            {
+                if (animatedStr.Length > animatedStrLength)
+                {
+                    animatedStrLength = animatedStr.Length;
+                    OnLetter();
+                }
+                textField.text = animatedStr;
+            })
+            .OnComplete(() => IsAnimatingText = false);
+    }
+
+    public void RushMessage(string message)
+    {
+        textField.DOKill();
+        textField.text = message;
+        IsAnimatingText = false;
+    }
+
+    private void OnLetter()
+    {
+        textField.transform.DOShakePosition(0.01f,1,25).SetEase(Ease.InOutSine).SetLoops(2, LoopType.Yoyo);
+    }
+}
